d7a22e3c801ad2a131f83c87c05db308fc393959
Gabriel Galeski <galeski35@gmail.com>
2023-06-07 17:16:54 -0300

    Realiza requisito 11

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,22 @@
--- Descomente e altere as linhas abaixo:
-
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
-
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+CREATE TABLE SpotifyClone.cancoes_fav(
+	user_id INT NOT NULL,
+	id_cancoes INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id, id_cancoes),
+    FOREIGN KEY (user_id) REFERENCES SpotifyClone.usuarios(user_id),
+    FOREIGN KEY (id_cancoes) REFERENCES SpotifyClone.cancoes(id_cancoes)
+) engine = InnoDB;
+ INSERT INTO SpotifyClone.cancoes_fav (user_id, id_cancoes)
+VALUES
+  ('1', '3'),
+  ('1', '6'),
+  ('1', '10'),
+  ('2', '4'),
+  ('3', '1'),
+  ('3', '3'),
+  ('4', '7'),
+  ('4', '4'),
+  ('5', '10'),
+  ('5', '2'),
+  ('8', '10'),
+  ('9', '7'),
+  ('10', '3')
